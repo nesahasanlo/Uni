bb9087aa0eac40dedb3a144de6a89ea9b7496b26
nesahasanlo <nesa.hasanloo5831@gmail.com>
2025-12-26 17:46:32 +0545

    Update Appointment.java

--- Appointment/Appointment.java
+++ Appointment/Appointment.java
@@ -1,11 +1,13 @@
 public class Appointment {
     String name;
-    String doctorname;
+    String doctorName;
     String time;
 
-    public Appointment(String name, String doctorname, String time){
+    public Appointment(String name, String doctorName, String time){
         this.name = name;
-        this.doctorname = doctorname;
+        this.doctorName = doctorName;
         this.time = time;
     }
 }
+
+
