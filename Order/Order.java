9d2684669dcff5a0019aca8c73a2c35fbbeea9f8
nesahasanlo <nesa.hasanloo5831@gmail.com>
2025-12-26 20:21:10 +0545

    Add files via upload

---
+++ Order/Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    String foodName;
+    int quantity;
+    double price;
+
+    public Order(String foodName, int quantity, double price){
+        this.foodName = foodName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
