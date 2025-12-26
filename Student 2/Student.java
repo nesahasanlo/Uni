a271321b23b3ac15a0c0861017e486ca83eaf2e8
nesahasanlo <nesa.hasanloo5831@gmail.com>
2025-12-26 20:41:58 +0545

    Update Student.java

--- Student 2/Student.java
+++ Student 2/Student.java
@@ -1,1 +1,13 @@
+public class Student {
+    public String name;
+    public String[] lesson;
+    public double[] grade;
 
+    public double average(){
+        double sum = 0;
+        for(int i = 0; i < grade.length; i++){
+            sum += grade[i];
+        }
+        return sum/lesson.length;
+    }
+}
